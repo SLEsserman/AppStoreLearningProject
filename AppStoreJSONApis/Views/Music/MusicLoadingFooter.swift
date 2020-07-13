ccec7fdff17fc1527f1d3e9351a21cd2d99519bb
Samuel Esserman <samessermanlambda@gmail.com>
2020-07-13 13:50:25 -0400

    Problems with the loading footer

---
+++ AppStoreJSONApis/Views/Music/MusicLoadingFooter.swift
@@ -0,0 +1,34 @@
+//
+//  MusicLoadingFooter.swift
+//  AppStoreJSONApis
+//
+//  Created by Samuel Esserman on 7/13/20.
+//  Copyright © 2020 Samuel Esserman. All rights reserved.
+//
+
+import UIKit
+
+class MusicLoadingFooter: UICollectionReusableView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let aiv             = UIActivityIndicatorView(style: .large)
+        aiv.color           = .darkGray
+        aiv.startAnimating()
+        
+        let label           = UILabel(text: "Loading More...", font: .systemFont(ofSize: 16))
+        label.textAlignment = .center
+        let stackView       = VerticalStackView(arrangedSubviews: [aiv, label], spacing: 20)
+        
+        addSubview(stackView)
+        stackView.centerInSuperview(size: .init(width: 200, height: 20))
+        
+        addSubview(aiv)
+        aiv.centerInSuperview()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
